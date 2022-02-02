a6b318f7511fa4d016882dd398550ba9a5ae4d66
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-02 13:36:08 +0600

    second commit

---
+++ add-multiply.js
@@ -0,0 +1,14 @@
+function add(num1,num2) {
+    const result = num1+num2;
+    return result;
+}
+const addTotal = add(5,8)
+console.log(addTotal);
+
+// multiplication
+function mul(num1,num2) {
+    const result = num1*num2;
+    return result;
+}
+const mulTotal = mul(5,8)
+console.log(mulTotal);
